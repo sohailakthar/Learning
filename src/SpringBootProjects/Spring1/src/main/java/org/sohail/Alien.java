3e0ec9a7946d53ada14f2499b66de3aeb5ab322a
sohailakthar <sohailakther2002@gmail.com>
2025-06-18 19:15:39 +0530

    Constructor injection - name, index, type

--- src/SpringBootProjects/Spring1/src/main/java/org/sohail/Alien.java
+++ src/SpringBootProjects/Spring1/src/main/java/org/sohail/Alien.java
@@ -3,10 +3,21 @@ package org.sohail;
 public class Alien {
 
     private int age;
+    private int salary;
     private Laptop laptop;
 
     public Alien() {
         System.out.println("Alien object created!");
+    }
+
+    //Parameterized constructor
+
+
+    public Alien(int age, int salary, Laptop laptop) {
+        System.out.println("Parameterized constructor called.");
+        this.age = age;
+        this.salary = salary;
+        this.laptop = laptop;
     }
 
     public void code() {
@@ -21,6 +32,14 @@ public class Alien {
     public void setAge(int age) {
         System.out.println("Setter called.");
         this.age = age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
     }
 
     public Laptop getLaptop() {
